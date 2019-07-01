4a295c99b0a5d3db0119a67dcc9c4a49fd761594
Eli <eli9acher@gmail.com>
2019-06-30 21:19:01 -0300

    cambio2

--- js/js.js
+++ js/js.js
@@ -31,9 +31,20 @@ function agregar(){
  
 
 function ordenartablanom(){
+    document.getElementById("personas").innerHTML=" "
 
-	ordenar(Personas)
-
+    Personas.sort(function(a, b){
+        if(a.nombre < b.nombre) { return -1; }
+        if(a.nombre > b.nombre) { return 1; }
+        return 0;
+    })
+    for(q=0;q<Personas.length; q++){
+        var todo = Personas[q].nombre + " " + Personas[q].edad + " " + Personas[q].mail
+        var n = document.createElement("LI");
+        var t = document.createTextNode(todo);
+        n.appendChild(t);
+        document.getElementById("personas").appendChild(n);
+     }
 }
 
 function ordenartablanum(){
@@ -83,26 +94,121 @@ function Informacionn(){
 
 function primerbody(){
 
+	var tabla =  document.getElementById("body1");
+	var o = document.createTextNode(Informacion.length).nodeValue;
+	var q = document.createTextNode(Informacion[Informacion.length-1].nombre).nodeValue;
+	var f = document.createTextNode(Informacion[Informacion.length-1].descripcion).nodeValue;
+	var n = document.createTextNode(Informacion[Informacion.length-1].motivo).nodeValue;
+	var z = document.createTextNode(Informacion[Informacion.length-1].duracion).nodeValue;
+	var x = document.createTextNode(Informacion[Informacion.length-1].celular).nodeValue;
+
+	
+	
+	
+
+
+	var columna = tabla.insertRow(-1); 
+	var cel1 = columna.insertCell(0); 
+	var cel2 = columna.insertCell(1); 
+	var cel3 = columna.insertCell(2); 
+	var cel4 = columna.insertCell(3); 
+	var cel5 = columna.insertCell(4); 
+	var cel6 = columna.insertCell(5); 
+	cel1.innerHTML = o; 
+	cel2.innerHTML = q;
+	cel3.innerHTML = f;
+	cel4.innerHTML = n;
+	cel5.innerHTML = z;
+	cel6.innerHTML = x;
+	
 
 
 
-	for(v=o;v<Informacion.length;v++){
+
+
+	/*celda.appendChild(o);
+	hilera.appendChild(celda);
+	document.getElementById("body1").append(celda);*/
+	
+
+
+	/*celda.appendChild(o);
+	hilera.appendChild(celda).insertrow(0)
+	
+	document.getElementById("body1").append(hilera)*/
+
+	/*document.getElementById("body1").append(p)
+	document.getElementById("body1").append(y)*/
+
+
+
+/*
+	
+	y.appendChild(o)
+	document.getElementById("body1").appendChild(y);
+
+	
+
+	y.appendChild(q);
+	document.getElementById("body1").appendChild(y);
+
+
+
+	y.appendChild(f);
+	document.getElementById("body1").appendChild(y);
+
+	y.appendChild(n);
+	document.getElementById("body1").appendChild(y);
+
+	y.appendChild(z);
+	document.getElementById("body1").appendChild(y);
+
+	y.appendChild(x);
+	document.getElementById("body1").appendChild(y);
+
+
+	
+
+
+
+
+
+	/*
 	var p = document.createElement("tr");  
-	var y = document.createElement("td")
-	let o = document.createTextNode(Informacion[v].nombre);
-	let o = document.createTextNode(Informacion[v].nombre);
-	let o = document.createTextNode(Informacion[v].nombre);
-	let o = document.createTextNode(Informacion[v].nombre);
-	let o = document.createTextNode(Informacion[v].nombre);
+	var y = document.createElement("td");
+	var o = document.createTextNode(Informacion.length);
+	var q = document.createTextNode(Informacion[v].nombre);
+	var f = document.createTextNode(Informacion[v].descripcion);
+	var n = document.createTextNode(Informacion[v].motivo);
+	var z = document.createTextNode(Informacion[v].duracion);
+	var x = document.createTextNode(Informacion[v].celular);
 
 	p.appendChild();
 	y.appendChild(o);
 	document.getElementById("body1").appendChild(y);
-	document.getElementById("body1").appendChild(p);
+	y.appendChild(q);
+	document.getElementById("body1").appendChild(y);
+	y.appendChild(f);
+	document.getElementById("body1").appendChild(y);
+	y.appendChild(n);
+	document.getElementById("body1").appendChild(y);
+	y.appendChild(z);
+	document.getElementById("body1").appendChild(y);
+	y.appendChild(y);
+	document.getElementById("body1").appendChild(y);
+	y.appendChild(x);
+	document.getElementById("body1").appendChild(y);
+	document.getElementById("body1").appendChild(p);*/
 
 
-	}
+
+
+
+
+
+
 }
+
 
 
 
